dcadce58c63b0e251545d3ff2d62cdde65069102
Tzach <tzach_ben@hotmail.com>
2017-12-21 16:34:05 +0200

    comit

---
+++ app/src/main/java/com/example/tzach/onclicklistner/Image_picker_Fragment.java
@@ -0,0 +1,32 @@
+package com.example.tzach.onclicklistner;
+
+
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Image_picker_Fragment extends DialogFragment {
+
+
+    public Image_picker_Fragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_image_picker_, container, false);
+
+        return rootView;
+    }
+
+}
--- app/src/main/java/com/example/tzach/onclicklistner/album_fragment.java
+++ app/src/main/java/com/example/tzach/onclicklistner/album_fragment.java
@@ -1,17 +1,21 @@
 package com.example.tzach.onclicklistner;
 
 
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class album_fragment extends Fragment {
+public class album_fragment extends Fragment implements View.OnClickListener {
+
+   Button btn1;
 
 
     public album_fragment() {
@@ -23,7 +27,30 @@ public class album_fragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_album_fragment, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_album_fragment, container, false);
+        btn1 = rootView.findViewById(R.id.creat_album_btn);
+        btn1.setOnClickListener(this);
+
+
+        return rootView;
     }
 
+
+    public void onClick(View v) {
+
+        Fragment fragment = null;
+        switch (v.getId()) {
+            case R.id.creat_album_btn:
+                fragment = new Image_picker_Fragment();
+                replaceFragment( (Image_picker_Fragment)fragment);
+                break;
+
+        }}
+
+    public void replaceFragment(Image_picker_Fragment fragment) {
+        FragmentTransaction transaction = getFragmentManager().beginTransaction();
+        transaction.replace(R.id.albumContainer, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
 }
