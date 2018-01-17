0ec73f1f8e7b88bf968c112428ad3b9a1564ba82
Tzach <tzach_ben@hotmail.com>
2018-01-17 21:43:42 +0200

    commit

---
+++ app/src/main/java/com/example/tzach/onclicklistner/utils/ResStatus.java
@@ -0,0 +1,9 @@
+package com.example.tzach.onclicklistner.utils;
+
+/**
+ * enum class that for the status of update resources process
+ */
+public enum ResStatus {
+    SUCCESS,
+    FAIL
+}
---
+++ backend/src/main/java/com/example/Tzach/myapplication/backend/objects/Albums.java
@@ -0,0 +1,8 @@
+package com.example.Tzach.myapplication.backend.objects;
+
+/**
+ * Created by Tzach on 1/17/2018.
+ */
+
+public class Albums {
+}
